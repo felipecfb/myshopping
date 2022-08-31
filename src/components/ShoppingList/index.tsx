d0a60d2e348e009c09137d995132fb6a3f57c3da
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-08-31 14:36:53 -0300

    ✨ feat: Consuming firestore collection of products

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -1,18 +1,33 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  useEffect(() => {
+    firestore()
+      .collection("products")
+      .get()
+      .then((response) => {
+        const data = response.docs.map((doc) => {
+          return {
+            ...doc.data(),
+            id: doc.id,
+          };
+        }) as ProductProps[];
+        setProducts(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
